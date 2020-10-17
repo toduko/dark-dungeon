fad3ca3ef2f0059d1fa63f97f56ea64c4a2cedca
toduko <teddyepich@gmail.com>
2020-10-17 10:22:20 +0300

    Initial Commit

---
+++ main/warrior/warrior.go
@@ -0,0 +1,35 @@
+components {
+  id: "warrior"
+  component: "/main/warrior/warrior.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/main/warrior/warrior.atlas\"\n"
+  "default_animation: \"warrior_idle\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
