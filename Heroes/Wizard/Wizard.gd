908b3a51a84db2ba5c7118597a91777494c0cdd0
toduko <teddyepich@gmail.com>
2020-11-23 10:40:52 +0200

    Added wizard attacks and made it extend player script

--- Heroes/Wizard/Wizard.gd
+++ Heroes/Wizard/Wizard.gd
@@ -1,16 +1,10 @@
-extends KinematicBody2D
+extends "res://Heroes/player.gd"
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	MOVEMENT_SPEED = 100
+	MAX_ATTACK_CD = 20
+	
+func handle_attack(attack_pressed):
+	if attack_pressed:
+		attack_cd = MAX_ATTACK_CD
+		$AnimatedSprite.play("attack")
